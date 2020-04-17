1d991ff3c2b1f308aff4c4556ec1949db7871ff2
Yuto Tokunaga <yuntan.sub1@gmail.com>
2020-04-17 23:20:59 +0900

    Nextcloud Login flow v2

--- window.js
+++ window.js
@@ -1,30 +1,61 @@
 'use strict';
 
+async function ncLoginFlow(domain) {
+  // Nextcloud Login flow v2
+  // https://docs.nextcloud.com/server/latest/developer_manual/client_apis/LoginFlow/index.html#login-flow-v2
+  const url = `https://${domain}/index.php/login/v2`;
+  const response = await fetch(url, { method: 'POST' });
+  const { poll: { token, endpoint }, login } = await response.json();
+
+  await browser.tabs.create({ url: login });
+
+  return await new Promise(resolve => {
+    // polling
+    const id = setInterval(async () => {
+      const params = new URLSearchParams();
+      params.append('token', token);
+      const response = await fetch(endpoint, { method: 'POST', body: params });
+      if (response.ok) {
+        const { loginName: username, appPassword: password } = await response.json();
+        clearInterval(id);
+        resolve({ server: 'nc', username, password });
+      }
+    }, 1000);
+  });
+}
+
 document.querySelector('#btnOk').addEventListener('click', async ev => {
   const btnOk = ev.target;
   btnOk.setAttribute('disabled', 'true');
 
-  const message = document.querySelector('#message');
-  message.innerText = 'Mounting...';
-
   const name = document.querySelector('#name').value;
-  const url = document.querySelector("#url").value;
+  const domain = document.querySelector("#domain").value;
   const username = document.querySelector("#username").value;
   const password = document.querySelector("#password").value;
-  // if (url.substring(url.length - 1) === "/") {
-  //     url = url.substring(0, url.length - 1);
-  // }
 
-  const request = {
-    type: 'mount',
-    authType: 'basic',
-    name, url, username, password,
-  };
+  const request = { server: 'oc', name, domain, username, password };
+
+  const details = document.querySelector('#expander');
+  if (!details.open) {
+    Object.assign(request, await ncLoginFlow(domain));
+  }
+
+  const message = document.querySelector('#message');
+  message.innerText = 'Mounting...';
 
   await browser.runtime.sendMessage(request)
     .then(window.close)
     .catch(error => {
+      console.error(error);
       message.innerText = error.message;
       btnOk.removeAttribute("disabled");
     });
+});
+
+document.querySelector('#expander').addEventListener('toggle', ev => {
+  const details = ev.target;
+  const button = document.querySelector('#btnOk');
+
+  if (details.open) button.innerText = 'Mount';
+  else button.innerText = 'Login';
 });
